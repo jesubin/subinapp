7ab5515fb033d29d1fec62f80e3fcf55cacfecb1
myname <dokdokorea01@naver.com>
2025-01-06 10:57:36 +0900

    Initial commit

---
+++ src/test/java/com/example/subinapp/SubinappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubinappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
